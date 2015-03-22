950104804cd2de66243784b630d4dca32a63e4f4
Andreas Möller <am@localheinz.com>
2015-03-22 18:27:57 +0100

    Enhancement: Add mutator and accessor for cache file to Config

--- Symfony/CS/Config/Config.php
+++ Symfony/CS/Config/Config.php
@@ -32,6 +32,7 @@ class Config implements ConfigInterface
     protected $usingCache = true;
     protected $usingLinter = true;
     protected $hideProgress = false;
+    protected $cacheFile = '.php_cs.cache';
 
     public function __construct($name = 'default', $description = 'A default configuration')
     {
@@ -156,5 +157,28 @@ class Config implements ConfigInterface
     public function usingLinter()
     {
         return $this->usingLinter;
+    }
+
+    /**
+     * Sets the path to the cache file.
+     *
+     * @param string $cacheFile
+     * @return self
+     */
+    public function setCacheFile($cacheFile)
+    {
+        $this->cacheFile = $cacheFile;
+
+        return $this;
+    }
+
+    /**
+     * Returns the path to the cache file.
+     *
+     * @return string
+     */
+    public function getCacheFile()
+    {
+        return $this->cacheFile;
     }
 }
